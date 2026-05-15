73f96eff9e6a745144cce5588329f778d7c67c5c
barancinar <cinarbaran2003@gmail.com>
2026-05-15 17:16:30 +0300

    Add onboarding and authentication screens with navigation links

--- app/index.tsx
+++ app/index.tsx
@@ -1,4 +1,5 @@
-import "@/global.css"
+import "@/global.css";
+import { Link } from "expo-router";
 import { Text, View } from "react-native";
 
 export default function App() {
@@ -7,6 +8,15 @@ export default function App() {
             <Text className="text-xl font-bold text-success">
                 Welcome to Nativewind!
             </Text>
+            <Link href="/onboarding" className="mt-4 px-4 py-2 bg-primary rounded">
+                <Text className="text-white">Go to OnBoarding</Text>
+            </Link>
+            <Link href="/(auth)/sign-in" className="mt-4 px-4 py-2 bg-primary rounded">
+                <Text className="text-white">Go to Sign in</Text>
+            </Link>
+            <Link href="/(auth)/sign-up" className="mt-4 px-4 py-2 bg-primary rounded">
+                <Text className="text-white">Go to Sign up</Text>
+            </Link>
         </View>
     );
 }
